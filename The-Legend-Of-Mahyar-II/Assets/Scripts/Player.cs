81a6912a52abff1bf1264b122df0216d68d62c0c
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-28 13:18:00 -0800

    Modified Player Script

--- The-Legend-Of-Mahyar-II/Assets/Scripts/Player.cs
+++ The-Legend-Of-Mahyar-II/Assets/Scripts/Player.cs
@@ -15,6 +15,7 @@ public class Player : Character
     public float groundRadius;
     public float immortalTime;
     public float climbSpeed;
+    public float playerShredder;
 
     // private variables
     private Vector3 startPos;
@@ -31,21 +32,56 @@ public class Player : Character
     public bool Jump { get; set; }
     public bool OnGround { get; set; }
     public bool OnHeart { get; set; }
-    
 
+    private void Awake()
+    {
+        healthStat.Initialize();
+    }
     // Use this for initialization
     public override void Start ()
     {
         // Calls the Character Class Start Function
         base.Start();
+        OnLadder = false;
+        OnHeart = false;
+        startPos = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        MyRigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	private void Update ()
     {
-		
-	}
+        FallingOffMapHandler();
+        HandleInput();
+    }
 
+    // Check to see if the player is dead
+    private void OnDead()
+    {
+        if (Dead != null) { Dead(); }
+    }
+
+    // If the player is dead then set his hp to 0
+    public override bool IsDead
+    {
+        get
+        {
+            if (healthStat.CurrentVal <= 0) { OnDead(); }
+            return healthStat.CurrentVal <= 0;
+        }
+    }
+
+    // Respawns the player when he dies
+    public override void Death()
+    {
+        MyRigidbody.velocity = Vector3.zero;
+        MyAnimator.SetTrigger("idle");
+        healthStat.CurrentVal = healthStat.MaxVal;
+        transform.position = startPos;
+    }
+
+    // handles the taking damage part of the player
     public override IEnumerator TakeDamage()
     {
         if (!immortal)
@@ -66,19 +102,60 @@ public class Player : Character
             }
         }
     }
-    public override bool IsDead
+
+    // Blinks the player on and off
+    private IEnumerator IndicateImmortal()
     {
-        get
+        while (immortal)
         {
-            if (healthStat.CurrentVal <= 0) { OnDead(); }
-            return healthStat.CurrentVal <= 0;
+            spriteRenderer.enabled = false;
+            yield return new WaitForSeconds(.1f);
+            spriteRenderer.enabled = true;
+            yield return new WaitForSeconds(.1f);
         }
     }
 
+   
 
-    private void OnDead()
+   
+    // the action button for the player
+    private void Use()
     {
-        if (Dead != null) { Dead(); } 
+        if (useable != null) { useable.Use(); }
     }
 
+    // if the player falls off the map, he respawns
+    private void FallingOffMapHandler()
+    {
+        if (!TakingDamage && !IsDead)
+        {
+            if (transform.position.y <= playerShredder) { Death(); }
+        }
+        HandleInput();
+    }
+
+    // handles player input
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !OnLadder) { MyAnimator.SetTrigger("jump"); }
+        if (Input.GetKeyDown(KeyCode.Z)) { MyAnimator.SetTrigger("attack"); }
+        if (Input.GetKeyDown(KeyCode.C)) { Use(); }
+        /*
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+
+        }
+        */
+    }
 }
