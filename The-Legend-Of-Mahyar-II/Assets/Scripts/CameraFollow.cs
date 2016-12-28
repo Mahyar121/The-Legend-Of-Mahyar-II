34f5381a03c32e509f399f885d67505eb46f37a9
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-27 19:45:33 -0800

    Added HP and UI

---
+++ The-Legend-Of-Mahyar-II/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float xMax, yMax, xMin, yMin, xPadding, yPadding;
+    public Transform target;
+	
+	private void LateUpdate ()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(target.position.x + xPadding, xMin, xMax),
+            Mathf.Clamp(target.position.y + yPadding, yMin, yMax),
+            transform.position.z
+            );
+    }
+}
---
+++ The-Legend-Of-Mahyar-II/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Stats stat;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        stat.Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
